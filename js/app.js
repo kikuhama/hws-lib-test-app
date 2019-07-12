3d7c7e5a35bcb8f85baf6b45dea21178df063982
Kikuchi ken <ken@dione.editorial.hamajima.co.jp>
2019-07-12 19:33:59 +0900

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+const HwsLibTestApp = {
+    CONTENT_ID: "test-contents",
+    DATA_KEY: "test-app",
+    
+    App: function() {
+    },
+};
+
+HwsLibTestApp.App.prototype.start = function() {
+    this.load();
+    const dataSaveHandler = this.save;
+    const form = document.getElementById("dataform");
+    const clearBtn = document.getElementById("clear_data");
+    form.addEventListener("submit", dataSaveHandler);
+    clearBtn.addEventListener("click", this.clearData);
+};
+
+HwsLibTestApp.App.prototype.clearData = function(e) {
+    if(e) {
+	e.preventDefault();
+    }
+    
+    const nameEle = document.getElementById("name");
+    const descriptionEle = document.getElementById("description");
+    nameEle.value = "";
+    descriptionEle.value = "";
+
+    HWS.setUserData({
+	content_id: HwsLibTestApp.CONTENT_ID,
+	key: HwsLibTestApp.DATA_KEY,
+	data: null,
+	success: function() {
+	    alert("Data cleared");
+	},
+	error: function(req, status, error) {
+	    alert("Data clear error: ", error);
+	}
+    });
+}
+
+HwsLibTestApp.App.prototype.load = function() {
+    const nameEle = document.getElementById("name");
+    const descriptionEle = document.getElementById("description");
+    
+    HWS.getUserData({
+	content_id: HwsLibTestApp.CONTENT_ID,
+	key: HwsLibTestApp.DATA_KEY,
+	success: function(data) {
+	    if(data) {
+		nameEle.value = data.name;
+		descriptionEle.value = data.description;
+	    }
+	},
+	error: function(req, status, error) {
+	}
+    });
+};
+
+HwsLibTestApp.App.prototype.save = function(e) {
+    if(e) {
+	e.preventDefault();
+    }
+    
+    const nameEle = document.getElementById("name");
+    const descriptionEle = document.getElementById("description");
+    const data = {
+	name: nameEle.value,
+	description: descriptionEle.value
+    };
+    HWS.setUserData({
+	content_id: HwsLibTestApp.CONTENT_ID,
+	key: HwsLibTestApp.DATA_KEY,
+	data: data,
+	success: function() {
+	    alert("Data saved");
+	},
+	error: function(req, status, error) {
+	    alert("Data save error: ", error);
+	}
+    });
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const app = new HwsLibTestApp.App();
+    app.start();
+});
